4b0c6ee812f9209d4b9564b1b61ca6c8741ac58f
Maurice Reigman <m_c.r@hotmail.com>
2017-01-15 21:45:07 +0100

    Refactor name

--- src/app/components/moviemanager-search-block/moviemanager-search-block.component.ts
+++ src/app/components/moviemanager-search-block/moviemanager-search-block.component.ts
@@ -4,10 +4,15 @@ import {MovieService} from "../../services/movie-service/movie-service.service";
 
 
 @Component({
-    selector: 'search-block-component',
+    selector: 'moviemanager-search-block',
     templateUrl: 'moviemanager-search-block.component.html',
     styleUrls: ['moviemanager-search-block.component.scss'],
     providers: [MovieService]
+    /**
+     * ASSIGNMENT 5.3
+     * our MovieService provider wil be used by sibling components, angulars scope prevents the instance to be shared at the moment because of the provider for the movie service being added here.
+     * ad it to the parent component in the component tree so the service will be available to it children and thus the sibling moviefinder-movie-list
+     */
 })
 export class MovieManagerSearchBlockComponent implements OnInit {
 
