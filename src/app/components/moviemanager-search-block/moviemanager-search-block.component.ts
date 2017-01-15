58c746d2d6ef2d80e41d1981d325dfb90859182c
Maurice Reigman <m_c.r@hotmail.com>
2017-01-15 17:46:38 +0100

    Refactor names

--- src/app/components/moviemanager-search-block/moviemanager-search-block.component.ts
+++ src/app/components/moviemanager-search-block/moviemanager-search-block.component.ts
@@ -3,14 +3,14 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'search-block-component',
-    templateUrl: 'search-block.component.html',
-    styleUrls: ['search-block.component.scss']
+    templateUrl: 'moviemanager-search-block.component.html',
+    styleUrls: ['moviemanager-search-block.component.scss']
 })
-export class SearchBlockComponent implements OnInit {
+export class MovieManagerSearchBlockComponent implements OnInit {
 
     /**
      * ASSIGNMENT 2.1:
-     * add a the private property "title" of type string to this "SearchBlockComponent class"
+     * add a the private property "title" of type string to this "MovieManagerSearchBlockComponent class"
      * @example: private myProperty: string;
      **/
 
