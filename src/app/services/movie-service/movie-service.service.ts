c032b25871b80eec28a65a9ca2b43fc0d30ad91c
Maurice Reigman <m_c.r@hotmail.com>
2017-01-16 19:00:05 +0100

    assigment 5 done

--- src/app/services/movie-service/movie-service.service.ts
+++ src/app/services/movie-service/movie-service.service.ts
@@ -21,19 +21,6 @@ export class MovieService extends MovieDataStoreService {
             .subscribe((data) => {
                 this.setData(data);
             });
-
-        /**
-         * ASSIGNMENT: 5.2:
-         * let our MovieService extend the MovieDataStoreService so we have the observable available
-         * subscribe to the http request here instead of in our component so we can stpre the data in the data store which we inherit by extending
-         * @example:
-         * http.get("www.api.nl")
-         *  .map(res => res.json())
-         *  .subscribe((data)=>{
-         *      this.setMyData(data);
-         *  });
-         */
-
     }
 
 }
